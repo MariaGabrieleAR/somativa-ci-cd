f2740eb932224236b0cd57ddbefd952f02455030
Maria Gabriele <gbialvees@gmail.com>
2024-04-16 17:15:44 -0300

    Add arquivos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Login(){
+  const[email, setEmail] = useState('');
+  const[senha, setSenha] = useState('');
+  const[mensagem, setMensagem] = useState('');
+
+
+const handleLogin = () => {
+  if(email === 'maria.gabriele@pucpr.br' && senha === '246810'){
+    setMensagem('Acessado com Sucesso!');
+  }else{
+    setMensagem('Usuário ou senha incorretos!')
+  }
 }
 
-export default App;
+return(
+  <div className="container">
+    <h2>Login</h2>
+    <input 
+    className="input"
+    type="email"
+    placeholder="E-mail"
+    value={email}
+    onChange={(e)=> setEmail(e.target.value)}/>
+    <br/>
+    <input
+    className="input"
+    type="password"
+    placeholder="Senha"
+    value={senha}
+    onChange={(e) => setSenha(e.target.value)}
+    />
+    <br/>
+    <button className="button" onClick={handleLogin}>Acessar</button>
+    <br/>
+    <label className="message" >{mensagem}</label>
+  </div>
+);
+
+}
+ 
+export default Login;
