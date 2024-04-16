e46c532de0baca966a4863126143761f9946ce59
Maria Gabriele <88037649+MariaGabrieleAR@users.noreply.github.com>
2024-04-16 18:00:06 -0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,9 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  // Procure pelo texto diretamente dentro do componente, sem expressão regular
+  const linkElement = screen.getByText('learn react', { exact: false });
+  // Use expect.toBeInTheDocument para verificar se o elemento está presente no DOM
   expect(linkElement).toBeInTheDocument();
 });
+
